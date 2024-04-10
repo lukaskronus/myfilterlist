82a68c7d5ec9da8c967ff3bf0b8e1dcf3ce4d989
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2024-04-10 14:01:40 +0700

    Update cf_zerotrust.sh

--- cf_zerotrust.sh
+++ cf_zerotrust.sh
@@ -1,13 +1,6 @@
 # Variables
 ruleID="38315ba5-f83a-495b-8953-2d95ab54e980"
 
-# Split to 1000 lines (Cloudflare Free Plan)
-wget -O raw.txt "https://raw.githubusercontent.com/lukaskronus/myfilterlist/main/domain/lists/cloudflare-justdomains.txt"
-split -l 1000 -d raw.txt blockedList
-mkdir -p final
-mv blockedList* final/
-cd final/
-
 # Remove all lists from Cloudflare Rules
 curl -X PUT "https://api.cloudflare.com/client/v4/accounts/$CF_ID/gateway/rules/$ruleID" \
     -H "X-Auth-Email: $CF_AC" \
@@ -28,37 +21,3 @@ jq -r -c '.result[].id' gatewayListJson | while read i; do
         -H "Authorization: $CF_TOKEN" \
         -H "Content-Type: application/json"
 done
-
-# Make Cloudflare-compatible list files
-for tempVar in blockedList*; do
-    jq -n '{name: $name, type: $type, items: $items}' --arg name "$tempVar" --arg type "DOMAIN" --argjson items "$(jq -R '[.,inputs] | map({value: .})' $tempVar)" > "$tempVar.json"
-
-    curl -X POST "https://api.cloudflare.com/client/v4/accounts/$CF_ID/gateway/lists" \
-        -H "X-Auth-Email: $CF_AC" \
-        -H "Authorization: $CF_TOKEN" \
-        -H "Content-Type: application/json" \
-        --data-binary "@${tempVar}.json"
-
-    rm "$tempVar.json"
-done
-
-# Get new list IDs
-curl -X GET "https://api.cloudflare.com/client/v4/accounts/$CF_ID/gateway/lists" \
-    -H "X-Auth-Email: $CF_AC" \
-    -H "Authorization: $CF_TOKEN" \
-    -H "Content-Type: application/json" > gatewayListJson
-
-# Generate new rule
-echo -n 'any(dns.content_category[*] in {1})' > rule1.json
-jq -r -c '.result[].id' gatewayListJson | while read i; do
-    echo -n " or any(dns.domains[*] in $"$i")" >> rule1.json
-done
-sed '$ s/-//g' rule1.json > rule2.json
-rule=$(head -n 1 rule2.json)
-
-# Apply rule
-curl -X PUT "https://api.cloudflare.com/client/v4/accounts/$CF_ID/gateway/rules/$ruleID" \
-    -H "X-Auth-Email: $CF_AC" \
-    -H "Authorization: $CF_TOKEN" \
-    -H "Content-Type: application/json" \
-    --data '{"name": "Block by Lists", "enabled": true,"action": "block","filters": ["dns"],"traffic": "'"$rule"'"}'
